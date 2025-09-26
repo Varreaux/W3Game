fbbb81563e1e366e9cd75f06f6ffee7d33ad51ec
Varreaux <116587683+Varreaux@users.noreply.github.com>
2025-09-26 17:31:04 -0400

    Initial commit

---
+++ breakable_door.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.name == "bullet":
+		area.queue_free()
+		$AnimatedSprite2D.play("breaking")  # Play the animation
+		await get_tree().create_timer(.3).timeout  # Wait 2 seconds
+		queue_free()  # This destroys the door
